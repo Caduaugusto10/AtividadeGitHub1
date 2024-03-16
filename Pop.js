3bda11807787ae2d540fb6ed24da24261c4fd8ca
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:05:22 -0300

    Método Pop

---
+++ Pop.js
@@ -0,0 +1,6 @@
+//A ferramenta pop() ele remove o último elemento de um array.
+
+let frutas = [ "Maracuja", "Abacaxi", "Goiaba", "Morango", "Pessego"];//Como o pessego ele é o ultimo ele será removido
+let listaFrutas = frutas.pop();
+
+console.log(frutas);
