6fb89521eeb3f00ac02a08ad67fdc29a36c65ccf
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:11:24 -0300

    Método Unshift

---
+++ Unshift.js
@@ -0,0 +1,7 @@
+//O unshift() ele adiciona um elemento ao inicio do array
+//Ao contrário do shift() que adiciona ao final de um array
+
+let tartarugasninjas = ['Raphael', 'Donatello', 'Michelangelo', 'Leonardo'];
+tartarugasninjas.unshift('MestreSplingter');
+
+console.log(tartarugasninjas);
