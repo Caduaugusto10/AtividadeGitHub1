36def85cae387a524919398d0f4fc4b34e5ea098
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:03:43 -0300

    Método forEach

---
+++ forEach.js
@@ -0,0 +1,7 @@
+//O forEach() ele já é um pouco mais complicado,
+//Pois ele irá executar uma função fornecida pelo usuário uma vez para cada elemento do array
+
+let numeros = [2, 4, 6, 8, 10];
+numeros.forEach(numeros => console.log(numeros * 2));//Ele pegará os numeros indicados e * (sinal de multiplicação em js) 
+//Multiplicará pelo número fornecido
+//Pode ser feito por multiplicação, subtração... Só alterar o sinal
