8b610c5ec8ecf7752e3251315bf37cdf0f6575df
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 19:58:37 -0300

    Método Concat

---
+++ Concat.js
@@ -0,0 +1,7 @@
+//O concat() ele faz uma concatenação (juntar) mais arrays 
+
+let Comidas = ['Hamburguer', 'Batata', 'Lasanha'];
+let adicionarComidas = ['Strogonoff', 'Carne'];
+let todasComidas = Comidas.concat(adicionarComidas);
+
+console.log(todasComidas);
