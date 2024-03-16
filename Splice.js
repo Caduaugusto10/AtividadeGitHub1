7b4bc35f907ff3d0de6ada465f59cc551f14f4b4
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:10:18 -0300

    Método Splice

---
+++ Splice.js
@@ -0,0 +1,6 @@
+//O splice() ele modifica um array removendo elementos existentes e/ou adiciona novos elementos
+
+let letras = ['A', 'B', 'C', 'E'];
+letras.splice(3, 2, 'D');
+
+console.log(letras);
