032397d26776674c8555ac7271f432e747b87fac
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:07:15 -0300

    Método Reverse

---
+++ Reverse.js
@@ -0,0 +1,7 @@
+//O reverse ele inverte a ordem dos elementos do array 
+//O último virará o primeiro e o primeiro virará o último
+
+let peixes = ['Tambaqui', 'Tilápia', 'Carpa', 'Pirarara'];
+peixes.reverse();
+
+console.log(peixes);
