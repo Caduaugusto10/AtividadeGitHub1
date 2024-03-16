f83980f98b9ed3777d4e3564fa6653dbc80b37cc
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:07:57 -0300

    Método Shift

---
+++ Shift.js
@@ -0,0 +1,7 @@
+//O shift() ele é quase igual ao pop(),
+//Porém ao invés de retirar o último elemento ele retira o primeiro
+
+let animais = ['Cachorro', 'Gato', 'Coelho', 'Papagaio', 'Gavião'];//Como o cachorro é o primeiro da tabela ele será excluído
+let tabelaAnimais = animais.shift();
+
+console.log(animais);
