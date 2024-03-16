d40d7727d521f3a9b49ab6c5f20cbcd4bd23523a
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:06:10 -0300

    Método Push

---
+++ Push.js
@@ -0,0 +1,8 @@
+//push ()
+//O push () ele adiciona um ou mais elementos ao final de um array e retorna o novo comprimento do array
+
+let números = [1, 3, 5, 7];
+
+números.push(9); //Ele vai adicionar "6" para o final do array
+
+console.log(números);
