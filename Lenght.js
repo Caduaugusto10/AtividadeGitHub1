749b607a59be9018b4b55db0c78c29d9d6adc1c4
Caduaugusto10 <cadu.augusto10@gmail.com>
2024-03-16 20:04:36 -0300

    Método Lenght

---
+++ Lenght.js
@@ -0,0 +1,7 @@
+//O Lenght é bem simples, ele mostra o número (Comprimento) de elementos presentes em um array
+
+let marcas = ['Nike', 'Adidas', 'Lacoste', 'Gucci', 'CalvinKlain'];
+let comprimento = marcas.length;
+//Como tem 5 marcas selecionadas o comprimento das marcas será 5 
+
+console.log(comprimento);
